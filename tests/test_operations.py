charettes/django-syzygy

commit 5d29c7133cc2987a3f8e48a4d3d32feae207458e

Properly implement deconstruct for custom operations.
The full import path must be specified for operations that are not part of
django.db.migrations.

syzygy/operations.py
@@ -123,6 +123,10 @@ class PreRemoveField(migrations.AlterField):
             )
         return "Set field %s of %s NULLable" % (self.name, self.model_name)
 
+    def deconstruct(self):
+        _, args, kwargs = super().deconstruct()
+        return f"{__name__}.{self.__class__.__name__}", args, kwargs
+
 
 class AddField(migrations.AddField):
     """
@@ -167,6 +171,10 @@ class AddField(migrations.AddField):
             return super().database_forwards(
                 app_label, schema_editor, from_state, to_state
             )
+
+    def deconstruct(self):
+        _, args, kwargs = super().deconstruct()
+        return f"{__name__}.{self.__class__.__name__}", args, kwargs
 
 
 class PostAddField(migrations.AlterField):
@@ -219,3 +227,7 @@ class PostAddField(migrations.AlterField):
             self.name,
             self.model_name,
         )
+
+    def deconstruct(self):
+        _, args, kwargs = super().deconstruct()
+        return f"{__name__}.{self.__class__.__name__}", args, kwargs

tests/test_operations.py
@@ -77,6 +77,20 @@ class AddFieldTests(OperationTestCase):
     def test_database_forwards_discard_default(self):
         self.test_database_forwards(preserve_default=False)
 
+    def test_deconstruct(self):
+        model_name = "TestModel"
+        field_name = "foo"
+        field = models.IntegerField(default=42)
+        operation = AddField(model_name, field_name, field)
+        self.assertEqual(
+            operation.deconstruct(),
+            (
+                "syzygy.operations.AddField",
+                [],
+                {"model_name": model_name, "name": field_name, "field": field},
+            ),
+        )
+
 
 class PostAddFieldTests(OperationTestCase):
     def test_database_forwards(
@@ -182,6 +196,20 @@ class PostAddFieldTests(OperationTestCase):
             "Drop database DEFAULT of field foo on TestModel",
         )
 
+    def test_deconstruct(self):
+        model_name = "TestModel"
+        field_name = "foo"
+        field = models.IntegerField(default=42)
+        operation = PostAddField(model_name, field_name, field)
+        self.assertEqual(
+            operation.deconstruct(),
+            (
+                "syzygy.operations.PostAddField",
+                [],
+                {"model_name": model_name, "name": field_name, "field": field},
+            ),
+        )
+
 
 class PreRemoveFieldTests(OperationTestCase):
     def test_database_forwards_null(self):
@@ -262,3 +290,22 @@ class PreRemoveFieldTests(OperationTestCase):
             PreRemoveField("TestModel", "foo", models.IntegerField()).describe(),
             "Set field foo of TestModel NULLable",
         )
+
+    def test_deconstruct(self):
+        model_name = "TestModel"
+        field_name = "foo"
+        field = models.IntegerField(default=42)
+        operations = PreRemoveField(
+            model_name,
+            field_name,
+            field,
+        )
+        self.assertEqual(
+            operations.deconstruct(),
+            (
+                "syzygy.operations.PreRemoveField",
+                [],
+                {"model_name": model_name, "name": field_name, "field": field},
+            ),
+        )
+

commit 6f289f16d504d996b1854f02bd3399a41793fef6

Properly deconstruct custom operations.
Operation.deconstruct is actually expected to return only the operation name
which contradicts the documented deconstruct behavior.

Regression introduced by 5d29c7133cc2987a3f8e48a4d3d32feae207458e.

syzygy/operations.py
@@ -121,10 +121,6 @@ class PreRemoveField(migrations.AlterField):
             )
         return "Set field %s of %s NULLable" % (self.name, self.model_name)
 
-    def deconstruct(self):
-        _, args, kwargs = super().deconstruct()
-        return f"{__name__}.{self.__class__.__name__}", args, kwargs
-
 
 class AddField(migrations.AddField):
     """
@@ -169,10 +165,6 @@ class AddField(migrations.AddField):
             return super().database_forwards(
                 app_label, schema_editor, from_state, to_state
             )
-
-    def deconstruct(self):
-        _, args, kwargs = super().deconstruct()
-        return f"{__name__}.{self.__class__.__name__}", args, kwargs
 
 
 class PostAddField(migrations.AlterField):
@@ -224,7 +216,3 @@ class PostAddField(migrations.AlterField):
             self.name,
             self.model_name,
         )
-
-    def deconstruct(self):
-        _, args, kwargs = super().deconstruct()
-        return f"{__name__}.{self.__class__.__name__}", args, kwargs

tests/test_operations.py
@@ -3,6 +3,7 @@ from typing import List, Optional, Tuple
 from django.db import connection, migrations, models
 from django.db.migrations.operations.base import Operation
 from django.db.migrations.optimizer import MigrationOptimizer
+from django.db.migrations.serializer import OperationSerializer
 from django.db.migrations.state import ProjectState
 from django.db.models.fields import NOT_PROVIDED
 from django.test import TestCase
@@ -85,11 +86,15 @@ class AddFieldTests(OperationTestCase):
         self.assertEqual(
             operation.deconstruct(),
             (
-                "syzygy.operations.AddField",
+                "AddField",
                 [],
                 {"model_name": model_name, "name": field_name, "field": field},
             ),
         )
+        serializer = OperationSerializer(operation)
+        serialized, imports = serializer.serialize()
+        self.assertTrue(serialized.startswith("syzygy.operations.AddField"))
+        self.assertIn("import syzygy.operations", imports)
 
 
 class PostAddFieldTests(OperationTestCase):
@@ -185,11 +190,15 @@ class PostAddFieldTests(OperationTestCase):
         self.assertEqual(
             operation.deconstruct(),
             (
-                "syzygy.operations.PostAddField",
+                "PostAddField",
                 [],
                 {"model_name": model_name, "name": field_name, "field": field},
             ),
         )
+        serializer = OperationSerializer(operation)
+        serialized, imports = serializer.serialize()
+        self.assertTrue(serialized.startswith("syzygy.operations.PostAddField"))
+        self.assertIn("import syzygy.operations", imports)
 
     def test_reduce(self):
         model_name = "TestModel"
@@ -276,19 +285,23 @@ class PreRemoveFieldTests(OperationTestCase):
         model_name = "TestModel"
         field_name = "foo"
         field = models.IntegerField(default=42)
-        operations = PreRemoveField(
+        operation = PreRemoveField(
             model_name,
             field_name,
             field,
         )
         self.assertEqual(
-            operations.deconstruct(),
+            operation.deconstruct(),
             (
-                "syzygy.operations.PreRemoveField",
+                "PreRemoveField",
                 [],
                 {"model_name": model_name, "name": field_name, "field": field},
             ),
         )
+        serializer = OperationSerializer(operation)
+        serialized, imports = serializer.serialize()
+        self.assertTrue(serialized.startswith("syzygy.operations.PreRemoveField"))
+        self.assertIn("import syzygy.operations", imports)
 
     def test_elidable(self):
         model_name = "TestModel"