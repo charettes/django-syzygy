e97f508ea0339856eaf00fb22d9c10cfd0fae98b
Simon Charette <simon.charette@zapier.com>
2023-05-11 00:51:32 -0400

    Release 0.1.0a11.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.rst") as file_:
 
 setup(
     name="django-syzygy",
-    version="0.1.0a10",
+    version="0.1.0a11",
     description="",
     long_description=long_description,
     long_description_content_type="text/x-rst",
